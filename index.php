a273aca5ee1cd550759e01c5f68b8a598b96ba9b
Daniel Mendoza <32989586+mr-dmtx@users.noreply.github.com>
2020-09-24 17:13:05 -0300

    Hello World

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Smart Search</title>
+	<?php
+		include 'php/links-head.php';
+	?>
+</head>
+<body>
+	<div class="container text-center">
+		<div class="mt-5">
+			<h1 class="font-weight-bold">Smart Search</h1>
+			<p>Desenvolvido por Daniel, Erick, Igor e Lucas</p>
+			<p id="links">
+				<a href="https://docs.google.com/document/d/1LkSrmaXsI_3GRNHHXb7fo43zPfwx_WERz3t-r7hu4CQ/edit">Documentação</a>
+				-
+				<a href="https://github.com/mr-dmtx/SmartSearch">Github</a>		
+			</p>
+		</div>
+		
+	</div>
+</body>
+</html>
---
+++ php/links-head.php
@@ -0,0 +1,3 @@
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-4.5.2-dist/css/bootstrap.min.css">	
+<link rel="stylesheet" type="text/css" href="css/index.css">
