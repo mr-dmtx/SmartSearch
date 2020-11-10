5074146225a015b349b0ef1100ac931fc2fc22b5
Daniel <estagio@saovicente.local>
2020-11-10 12:27:11 -0300

    Ajustes atualizar dados

--- perfil/index.php
+++ perfil/index.php
@@ -31,126 +31,147 @@ try {
   $op = $_POST['submit'] ?? null;
 
   if(!is_null($op)){
-    try {
-      $email = $_POST['email'];
-      switch ($op) {
 
-        case 'att':
-        if ($id%2 == 0) {
-          if(verificarEmail($email)){
-            $senha = $_POST['senha-atual'] ?? null;
-            if(!is_null($senha)){
-              $senhaNova = $_POST['senha-nova'] ?? null;
-              $reSenhaNova = $_POST['re-senha-nova'] ?? null;
+    $email = $_POST['email'];
+    switch ($op) {
 
-              if(!is_null($senhaNova) and !is_null($reSenhaNova)){
-                $nomeUsuario = $_POST['nome'];
-                if($senhaNova == $reSenhaNova and !is_null($nomeUsuario))
-                {
-                //alterar dados conta
-                  $update = "UPDATE usuario SET nm_email = '$email', cd_senha = '$senhaNova', nm_usuario = '$nomeUsuario'";
-
-                  $cmd = $conexao->prepare($update);
-
-                  $cmd->execute();
-                }
-                else{
-                  $aviso = "As senhas não conferem";
-                }
-              }
-            }
+      case 'att':
+        if($email != $_SESSION['email']){
+          if(!verificarEmail($email)){
+            $email = $_SESSION['email'];
           }
-          else{
-            $aviso = 'Esse email já esta em uso!';
+          else{ 
+            $aviso = "Esse email já esta em uso!";
           }
         }
 
-        break;
+        $senha = $_POST['senha-atual'];
+        $senhaNova = $_POST['senha-nova'];
+        $reSenhaNova = $_POST['re-senha-nova'];
+        $nome = $_POST['nome'];
+        if($senhaNova == $reSenhaNova){
+          $senha = md5($senhaNova);
+        }
 
-        case 'delete':
-        $delete = "DELETE FROM usuario WHERE cd_usuario = :id";  
+        $update = "UPDATE usuario SET nm_email = ?, cd_senha = ?, nm_usuario = ? WHERE cd_usuario = ?";
 
-        $cmd = $conexao->prepare($delete);
+        $cmd = $conexao->prepare($update);
 
-        $cmd->bindValue(":id", $_SESSION['id']);
+        $cmd->bindParam(1, $email);
+        $cmd->bindParam(2, $senha);
+        $cmd->bindParam(3, $nome);
+        $cmd->bindParam(4, $_SESSION['id']);
 
         $cmd->execute();
 
-        header("location: ../php/logout.php"); 
+        if($_SESSION['id']%2 == 1){
 
-        break;
-      }
+          $update = "UPDATE endereco SET cd_cep = ?, nm_endereco = ?, cd_numero_endereco = ?, nm_cidade = ?, nm_bairro = ?, cd_complemento = ?, sg_uf = ? from endereco join usuario u on u.cd_usuario = l.fk_usuario_loja join loja l on e.fk_loja_endereco = l.cd_cnpj where cd_usuario = ?";
 
+          $cmd->prepare($update);
 
-    } catch (PDOException $e) {
-      $aviso = "Erro ao deletar conta. ". $e->getMessage();
+          $cmd->bindParam(1, $_POST['cep']);
+          $cmd->bindParam(2, $_POST['rua']);
+          $cmd->bindParam(3, $_POST['numero']);
+          $cmd->bindParam(4, $_POST['cidade']);
+          $cmd->bindParam(5, $_POST['bairro']);
+          $cmd->bindParam(6, $_POST['complemento']);
+          $cmd->bindParam(7, $_POST['uf']);
+          $cmd->bindParam(8, $_SESSION['id']);
+
+          $cmd->execute();
+        }
+
+        header("location: #"); 
+      break;
+
+      case 'delete':
+      $delete = "DELETE FROM usuario WHERE cd_usuario = :id";  
+
+      $cmd = $conexao->prepare($delete);
+
+      $cmd->bindValue(":id", $_SESSION['id']);
+
+      $cmd->execute();
+
+      header("location: ../php/logout.php"); 
+
+      break;
     }
 
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Smart Search</title>
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-   </head>
-   <body>
-    <style type="text/css">
-      label{
-        font-weight: 700;
-      }
-    </style>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Smart Search</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-          <a class="nav-link" href="../perfil/index.php"> Perfil</a>
-          <a class="nav-link" href='../php/logout.php'>Sair</a>
-        </div>
+
+  } 
+}catch (PDOException $e) {
+  $aviso = "Erro ao realizar operação. ". $e->getMessage();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Smart Search</title>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+  <style type="text/css">
+    label{
+      font-weight: 700;
+    }
+  </style>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Smart Search</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" href="../inicio">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="../perfil/index.php"> Perfil</a>
+        <a class="nav-link" href='../php/logout.php'>Sair</a>
       </div>
-    </nav>
-    <div class="container">
-      <h1 class="text-center mt-3">Meu Perfil #<?=$_SESSION['id']?></h1>
-      <form class="row justify-content-center" method="POST">
-        <div class="form-group col-md-6">
-          <label>Nome:</label>
-          <input type="text" class="form-control" name="nome" value="<?=$dados[0]['nm_usuario']?>">
-        </div>
-        <div class="w-100"></div>
-        <div class="form-group col-md-6">
-          <label>Email:</label>
-          <input type="email" class="form-control" name="email" value="<?=$dados[0]['nm_email']?>">
-        </div>
-        <div class="w-100"></div>
-        <div class="form-group col-md-6">
-          <label>Senha atual:</label>
-          <input type="password" class="form-control" name="senha-atual">
-        </div>
-        <div class="w-100"></div>
-        <div class="form-group col-md-6">
-          <label>Nova senha:</label>
-          <input type="password" class="form-control" name="senha-nova">
-        </div>
-        <div class="w-100"></div>
-        <div class="form-group col-md-6">
-          <label>Repita a nova senha:</label>
-          <input type="password" class="form-control" name="re-senha-nova">
-        </div>
-        <?php 
-        if($_SESSION['id']%2 == 1){
-          require 'loja-form.php';
-        }
-        ?>
-        <div class="w-100"></div>
-        <button type="submit" class="btn btn-primary col-md-5 mb-2" name="submit" value="att">Atualizar dados</button>
-        <div class="w-100"></div>
-        <button type="submit" class="btn btn-danger col-md-5 mb-5" name="submit" value="delete">Deletar conta</button>
-      </form>
     </div>
-  </body>
-  </html>
+  </nav>
+  <div class="container">
+    <h1 class="text-center mt-3">Meu Perfil #<?=$_SESSION['id']?></h1>
+    <span class="text-center" style="color: red">
+      <?=$aviso?>
+    </span>
+    <form class="row justify-content-center" method="POST">
+      <div class="form-group col-md-6">
+        <label>Nome:</label>
+        <input type="text" class="form-control" name="nome" value="<?=$dados[0]['nm_usuario']?>">
+      </div>
+      <div class="w-100"></div>
+      <div class="form-group col-md-6">
+        <label>Email:</label>
+        <input type="email" class="form-control" name="email" value="<?=$dados[0]['nm_email']?>">
+      </div>
+      <div class="w-100"></div>
+      <div class="form-group col-md-6">
+        <label>Senha atual:</label>
+        <input type="password" class="form-control" name="senha-atual" value="<?=$dados[0]['cd_senha']?>" readonly>
+      </div>
+      <div class="w-100"></div>
+      <div class="form-group col-md-6">
+        <label>Nova senha:</label>
+        <input type="password" class="form-control" name="senha-nova">
+      </div>
+      <div class="w-100"></div>
+      <div class="form-group col-md-6">
+        <label>Repita a nova senha:</label>
+        <input type="password" class="form-control" name="re-senha-nova">
+      </div>
+      <?php 
+      if($_SESSION['id']%2 == 1){
+        require 'loja-form.php';
+      }
+      ?>
+      <div class="w-100"></div>
+      <button type="submit" class="btn btn-primary col-md-5 mb-2" name="submit" value="att">Atualizar dados</button>
+      <div class="w-100"></div>
+      <button type="submit" class="btn btn-danger col-md-5 mb-5" name="submit" value="delete">Deletar conta</button>
+    </form>
+  </div>
+</body>
+</html>
--- perfil/loja-form.php
+++ perfil/loja-form.php
@@ -6,12 +6,12 @@
   <div class="w-100"></div>
   <div class="form-group col-md-6">
     <label>CEP:</label>
-    <input type="text" class="form-control" name="cep" value="<?=$dados[0]['cd_cep']?>">
+    <input type="text" onblur="pesquisacep(this.value)" class="form-control" name="cep" value="<?=$dados[0]['cd_cep']?>">
   </div>
   <div class="w-100"></div>
   <div class="form-group col-md-6">
     <label>Endereço:</label>
-    <input type="text" class="form-control" name="endereco" value="<?=$dados[0]['nm_endereco']?>">
+    <input type="text" class="form-control" name="rua" id="rua" value="<?=$dados[0]['nm_endereco']?>">
   </div>
   <div class="w-100"></div>
   <div class="form-group col-md-6">
@@ -21,12 +21,12 @@
   <div class="w-100"></div>
   <div class="form-group col-md-6">
     <label>Cidade:</label>
-    <input type="text" class="form-control" name="cidade" value="<?=$dados[0]['nm_cidade']?>">
+    <input type="text" class="form-control" name="cidade" id="cidade" value="<?=$dados[0]['nm_cidade']?>">
   </div>
   <div class="w-100"></div>
   <div class="form-group col-md-6">
     <label>Bairro:</label>
-    <input type="text" class="form-control" name="bairro" value="<?=$dados[0]['nm_bairro']?>">
+    <input type="text" class="form-control" name="bairro" id="bairro" value="<?=$dados[0]['nm_bairro']?>">
   </div>
   <div class="w-100"></div>
   <div class="form-group col-md-6">
@@ -36,11 +36,78 @@
   <div class="w-100"></div>
   <div class="form-group col-md-6">
     <label>UF:</label>
-    <input type="text" class="form-control" name="uf" value="<?=$dados[0]['sg_uf']?>">
+    <input type="text" class="form-control" name="uf" id="uf" value="<?=$dados[0]['sg_uf']?>">
   </div>
   <div class="w-100"></div>
   <div class="form-group col-md-6">
     <label>Telefone:</label>
     <input type="text" class="form-control" name="telefone" value="<?=$dados[0]['cd_telefone']?>">
   </div>
-  
+  <script>
+    function limpa_formulário_cep() {
+            //Limpa valores do formulário de cep.
+            document.getElementById('rua').value=("");
+            document.getElementById('bairro').value=("");
+            document.getElementById('cidade').value=("");
+            document.getElementById('uf').value=("");
+            document.getElementById('ibge').value=("");
+    }
+
+    function meu_callback(conteudo) {
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            document.getElementById('rua').value=(conteudo.logradouro);
+            document.getElementById('bairro').value=(conteudo.bairro);
+            document.getElementById('cidade').value=(conteudo.localidade);
+            document.getElementById('uf').value=(conteudo.uf);
+            //document.getElementById('ibge').value=(conteudo.ibge);
+        } //end if.
+        else {
+            //CEP não Encontrado.
+            limpa_formulário_cep();
+            alert("CEP não encontrado.");
+        }
+    }
+    function pesquisacep(valor) {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = valor.replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                document.getElementsByName('rua').value="...";
+                document.getElementsByName('bairro').value="...";
+                document.getElementsByName('cidade').value="...";
+                document.getElementsByName('uf').value="...";
+                //document.getElementById('ibge').value="...";
+
+                //Cria um elemento javascript.
+                var script = document.createElement('script');
+
+                //Sincroniza com o callback.
+                script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+                //Insere script no documento e carrega o conteúdo.
+                document.body.appendChild(script);
+
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    };
+  </script>
